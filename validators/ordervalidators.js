a7827311a9288e645bdab333404abf5f5c6094d7
BnXhamss <abdallahshameel@gmail.com>
2025-05-20 14:30:34 +0000

    eleventh commit

--- validators/ordervalidators.js
+++ validators/ordervalidators.js
@@ -2,7 +2,7 @@ import Joi from "joi";
  
 export const createOrderValidator = Joi.object({
     customersName: Joi.string().required(),
-    customersEmail: Joi.string().email().required(),
+    customersEmail: Joi.string().email().optional(),
     customersPhone: Joi.string().required(),
     OrderDescription: Joi.array().items(
         Joi.object({
